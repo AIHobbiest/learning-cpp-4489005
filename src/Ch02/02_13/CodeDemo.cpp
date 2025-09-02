e2f540c3ef4c1f1b666331edba51f3e1d6e3e0df
AIHobbiest <ahmedwaqar.dr@gmail.com>
2025-09-02 23:40:49 +0000

    completed the array printing

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -6,9 +6,13 @@
 
 int main(){
     int nums[5] = {1,23,32,24,337};
-    float result; 
+    float result = 0.0f; 
 
     // Write your code here
+    for (int i = 0; i < sizeof(nums)/sizeof(*nums); i++){
+        result += nums[i];
+
+    }
     
     std::cout << "The average is " << result << std::endl;
     
