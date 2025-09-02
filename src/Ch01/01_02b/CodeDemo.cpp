e92f2a7c51abb607f0baf86bdd18563bd834635c
AIHobbiest <ahmedwaqar.dr@gmail.com>
2025-09-02 20:04:13 +0000

    Finished first exercise

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,19 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string str;
+    std::cout << "Enter the name!" << std::endl;
+    std::cin >> str;
+    std::cout << "Entered name:" << std::endl;
+    std::cout << str;
+
+    std::cout << std::endl << std::endl;
+
+    return (0);
+
+}
