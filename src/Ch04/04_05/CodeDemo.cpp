92c1b3adc219579c919a364d18e77ddb9be1d6cd
AIHobbiest <ahmedwaqar.dr@gmail.com>
2025-09-03 19:20:29 +0000

    Completed GPA calculation is completed

--- src/Ch04/04_05/CodeDemo.cpp
+++ src/Ch04/04_05/CodeDemo.cpp
@@ -26,9 +26,42 @@ int main(){
 
     // Calculate the GPA for the selected student.
     // Write your code here
+    float c_pts = 0.0f;
+
+    for (auto x : grades) {
+        if (x.get_student_id() == id){
+        switch (x.get_grade()){
+            case 'A':
+                for (auto c : courses){
+                    if (c.get_id() == x.get_course_id()){
+                        c_pts += (c.get_credits() * 4);
+                    }
+            }
+                break;
+            case 'B':
+                for (auto c : courses){
+                    if (c.get_id() == x.get_course_id()){
+                        c_pts += (c.get_credits() * 3);
+                    }
+            }
+                break;
+            case 'C':
+                for (auto c : courses){
+                    if (c.get_id() == x.get_course_id()){
+                        c_pts += (c.get_credits() * 2);
+                    }
+            }
+                break;
+            default:
+                std::cout << "nothing is matched" << " " << std::endl;
+        } 
+    }
+    }
+
+    GPA = c_pts / (5+4+3);
 
     std::string student_str;
-    student_str = students[0].get_name(); // Change this to the selected student's name
+    student_str = students[id - 1].get_name(); // Change this to the selected student's name
 
     std::cout << "The GPA for " << student_str << " is " << GPA << std::endl;
     
